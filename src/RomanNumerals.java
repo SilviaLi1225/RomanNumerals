98ca5b355c1ebe12553c2aa5349c03428b148374
SilviaLi1225 <916621497@qq.com>
2018-10-17 18:56:08 +0300

    Assignment not finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,75 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RomanNumerals {
+	Map<String, Integer> map = new HashMap<String, Integer>();
+	
+    public RomanNumerals(){
+    	map.put("I", 1);
+    	map.put("V", 5);
+    	map.put("X", 10);
+    	map.put("L", 50);
+    	map.put("C", 100);
+    	map.put("D", 500);
+    	map.put("M", 1000);
+    }
+    
+	public boolean isValid(String romanNum){
+		
+		boolean result= true;
+		int count1 = 0;
+		int count2 = 0;
+		int count3 = 0;
+		int count4 = 0;
+		int countv = 0;
+		int countl = 0;
+		int countd = 0;
+		char[] roman = romanNum.toCharArray(); 
+		
+		for(int i=0; i<roman.length; i++){
+			if(roman[i] == 'I'){
+				count1++;
+			}
+			if(roman[i] == 'X'){
+				count2++;
+			}
+			if(roman[i] == 'C'){
+				count3++;
+			}
+			if(roman[i] == 'M'){
+				count4++;
+			}
+			if(roman[i] == 'V'){
+				countv++;
+			}
+			if(roman[i] == 'L'){
+				countl++;
+			}
+			if(roman[i] == 'D'){
+				countd++;
+			}
+		}
+		
+		if(count1>3 || count2>3 || count3>3 ||count4>3){
+			result = false;
+		}
+		
+		if(countv>1 || countl>1 || countd>1){
+			result = false;
+		}
+		return result;
+	}
+	
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		int result = 0;
+		if(romanNum.length() == 1){
+			result = map.get(romanNum);
+		}
+		
+		
+		return result;
 		
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -5,8 +5,31 @@ import org.junit.Test;
 public class TestRomanNumerals {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_isValid_I_1(){
+		//Arrange
+		RomanNumerals rm = new RomanNumerals();
+		//Act
+		assertEquals(false, rm.isValid("IIICI"));
 	}
+	
+	@Test
+	public void test_isValid_V_2(){
+		//Arrange
+		RomanNumerals rm = new RomanNumerals();
+		//Act
+		assertEquals(false,rm.isValid("VV"));
+	}
+	
+	
+	@Test
+	public void test_1() {
+		//Arrange
+		RomanNumerals rm = new RomanNumerals();
+		//Act
+		int result = rm.convertToInteger("I");
+		assertEquals(1,result);
+		
+	}
+	
 
 }
